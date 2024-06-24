e8b4ea97620be4f2979176683c5e3614399e08af
code-dust <51427114+code-dust@users.noreply.github.com>
2024-06-24 09:55:45 +0100

    added expressjs

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
 const http = require('http');
-
-const server = http.createServer((req,res)=> {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Node Page</title></head>');
-    res.write('<body><h1>hello node</h1></body>');
-    res.write('</html>');
-    res.end();
-});
-
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
 server.listen(3000);
