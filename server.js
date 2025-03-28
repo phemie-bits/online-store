22f1e8a0c3cf76ff429c0ae5d65ab8a723ab6eb7
unknown <folarniyi@gmail.com>
2025-03-28 23:29:24 +0100

    filebased database used

--- server.js
+++ server.js
@@ -1,5 +1,32 @@
-const http = require('http');
+// const express = require("express");
+// const app = express();
+
+// app.use('/',(req, res, next) => {
+//   console.log("In the middleware again");
+//   res.send("<h1>Hello from express</h1>");
+// });
+// app.listen(3000);
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 const app = express();
-const server = http.createServer(app);
-server.listen(3000);
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
